2903bb473dfe1d6ff2c87a03f8d10263581e08f5
Revertigo <dekel241@gmail.com>
2020-12-15 01:06:59 +0200

    Add new IPathFinder interface in order to choose shortest path algorithm at run time

---
+++ Assets/Scripts/6-algorithm/IPathFinder.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPathFinder<NodeType>
+{
+    List<NodeType> GetPath(IGraph<NodeType> graph, NodeType startNode, NodeType endNode, int maxiterations = 1000);
+}
