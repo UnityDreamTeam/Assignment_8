95f163279814fbe2091e434a5673579c047914f1
Revertigo <dekel241@gmail.com>
2020-12-14 22:42:47 +0200

    Refactor code to avoid abstraction breaking

--- Assets/Scripts/dijkstra/Node.cs
+++ Assets/Scripts/dijkstra/Node.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
+public class Node<NodeType>
 {
-    private Vector3Int position;
+    private NodeType position;
     private int distnace;
-    private Vector3Int father;//Father vertex, in order to reconstruct the path
+    private NodeType father;//Father vertex, in order to reconstruct the path
 
-    public Node(Node other) : this(other.Position, other.Distnace, other.Father) { }
-    public Node(Vector3Int position, int distance, Vector3Int father)
+    public Node(Node<NodeType> other) : this(other.Position, other.Distnace, other.Father) { }
+    public Node(NodeType position, int distance, NodeType father)
     {
         this.Position = position;
         this.Distnace = distance;
@@ -17,6 +17,6 @@ public class Node
     }
 
     public int Distnace { get => distnace; set => distnace = value; }
-    public Vector3Int Position { get => position; set => position = value; }
-    public Vector3Int Father { get => father; set => father = value; }
+    public NodeType Position { get => position; set => position = value; }
+    public NodeType Father { get => father; set => father = value; }
 }
