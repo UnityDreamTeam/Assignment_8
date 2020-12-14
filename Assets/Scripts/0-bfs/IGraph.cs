1ed3d70c0ce3ae4a58fbd126088ea111ebfd8743
Revertigo <dekel241@gmail.com>
2020-12-15 01:08:01 +0200

    Fix comment

--- Assets/Scripts/0-bfs/IGraph.cs
+++ Assets/Scripts/0-bfs/IGraph.cs
@@ -3,7 +3,8 @@
 /**
  * An abstract graph.
  * It does not use any memory.
- * It only has a single abstract function Neighbors, that returns the neighbors of a given node.
+ * It has two single abstract function Neighbors, and Edges that returns the
+ * neighbors and edges of a given node, respectively.
  * T = type of node in graph.
  * @author Erel Segal-Halevi
  * @since 2020-12
